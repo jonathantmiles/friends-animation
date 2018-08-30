39ceedabf00365b5f74c15a6ed0192c1dc59c83e
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-08-30 14:24:15 -0700

    begins to implement animations, incuding commented out headings for each expected section

--- Friends Animation/Animation/ImageTransitionAnimator.swift
+++ Friends Animation/Animation/ImageTransitionAnimator.swift
@@ -16,20 +16,47 @@ class ImageTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         
+        // establish destination and origin ViewControllers as well as create a view from the destination view to use during animation
         guard let fromVC = transitionContext.viewController(forKey: .from) as? FriendsTableViewController,
             let toVC = transitionContext.viewController(forKey: .to) as? FriendDetailViewController,
             let toView = transitionContext.view(forKey: .to) else { return }
         
+        // get the containerView out so the animation can be mocked up onto it
         let containerView = transitionContext.containerView
         
+        // hopefully pulls the correct cell out from the tableView so we can get to its properties
         guard let indexPath = fromVC.tableView.indexPathForSelectedRow,
             let cell = fromVC.tableView.cellForRow(at: indexPath) as? FriendsTableViewCell else { return }
         
-        let sourcePhotoView = cell.friendPicImage
-        let destinationPhotoView = toVC.friendPicLageImage
+        // set up view for transition purposes
+        let toEndFrame = transitionContext.finalFrame(for: toVC)
         
-        let sourceNameLabel = cell.friendNameLabel
-        let destinationNameLabel = toVC.nameLabel
+        
+        // declare original elements & implicitly unwrap
+        let sourcePhotoView = cell.friendPicImage!
+        let destinationPhotoView = toVC.friendPicLageImage!
+        
+        let sourceNameLabel = cell.friendNameLabel!
+        let destinationNameLabel = toVC.nameLabel!
+        
+        // hide original elements
+        sourcePhotoView.alpha = 0.0
+        destinationPhotoView.alpha = 0.0
+        
+        sourceNameLabel.alpha = 0.0
+        destinationNameLabel.alpha = 0.0
+        
+        // setup transition view by adding dummy copies of moving elements to it
+        
+        // dummy label
+        
+        // dummy image
+        
+        
+        // setup animation by adding the dummy elements to the animation
+        // remember to restore opacity of original elements
+        
+        
         
         let startLabelFrame = transitionContext.initialFrame(for: fromVC)
         containerView.addSubview(toView)
